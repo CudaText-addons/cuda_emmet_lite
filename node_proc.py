698b5365abeaf8bc5ae1dde1431069f4e31e9316
Alexey-t <support@uvviewsoft.com>
2016-09-09 16:17:29 +0300

    fix for Mac

--- node_proc.py
+++ node_proc.py
@@ -1,7 +1,17 @@
 import os
+import platform
 import subprocess
 
 MSG_CANNOT_RUN_NODE = "Cannot run Node.js. Make sure it's in your PATH."
+
+NODE_FILE = 'node'
+if os.name=='posix':
+    s = platform.system()
+    if s=='Linux':
+        NODE_FILE = 'nodejs' 
+    elif s=='Darwin':
+        NODE_FILE = '/usr/local/bin/node'
+
 
 def run_node(text, params_list):
     enc = 'utf8'
@@ -10,7 +20,7 @@ def run_node(text, params_list):
         startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         startupinfo.wShowWindow = subprocess.SW_HIDE
         try:
-            p = subprocess.Popen(['node'] + params_list, 
+            p = subprocess.Popen([NODE_FILE] + params_list, 
               startupinfo=startupinfo, 
               stdout=subprocess.PIPE, 
               stdin=subprocess.PIPE, 
@@ -19,7 +29,7 @@ def run_node(text, params_list):
             raise Exception(MSG_CANNOT_RUN_NODE)
     else:
         try:
-            p = subprocess.Popen(['nodejs'] + params_list, 
+            p = subprocess.Popen([NODE_FILE] + params_list, 
               stdout=subprocess.PIPE, 
               stdin=subprocess.PIPE, 
               stderr=subprocess.PIPE)
