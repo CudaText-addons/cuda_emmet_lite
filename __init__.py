e3cfc30d793d1938fac848fbd030f13d8f2f8277
Alexey T. <support@uvviewsoft.com>
2025-04-02 17:23:42 +0300

    trim spaces

--- __init__.py
+++ __init__.py
@@ -20,14 +20,14 @@ def get_syntax():
     lexer = ed.get_prop(PROP_LEXER_CARET)
     if lexer in lexers_xml:
         return 'xsl'
-    elif lexer in lexers_css: 
+    elif lexer in lexers_css:
         return 'css'
     else:
         return 'html'
-    
+
 def get_profile():
     return ini_read(fn_ini, ini_section, ini_key_profile, profiles[0])
-    
+
 
 def do_find_expand():
     x, y, x1, y1 = ed.get_carets()[0]
@@ -41,8 +41,8 @@ def do_find_expand():
     except Exception as e:
         #msg_status(str(e))
         return
-    
-    
+
+
 def do_insert_result(x0, y0, x1, y1, text, text_insert):
     if text_insert:
         for i in [1,2,3,4,5,6,7,8,9,0]:
@@ -50,17 +50,17 @@ def do_insert_result(x0, y0, x1, y1, text, text_insert):
             if text_rep in text:
                 text = text.replace(text_rep, '${'+str(i)+':'+text_insert+'}', 1)
                 break
-    
+
     ed.delete(x0, y0, x1, y1)
     ed.set_caret(x0, y0)
-    
+
     lines = text.splitlines()
     insert_snip_into_editor(ed, lines)
 
 
 def do_expand_abbrev(text_ab):
     msg_status('Expanding: %s (profile %s)' % (text_ab, get_profile()))
-        
+
     try:
         text = run_node('', [fn_script, 'expand', text_ab, get_syntax(), get_profile() ])
     except Exception as e:
@@ -70,7 +70,7 @@ def do_expand_abbrev(text_ab):
     if not text or text=='?':
         msg_status('Cannot expand Emmet abbreviation: '+text_ab)
         return
-        
+
     return text
 
 
@@ -92,28 +92,28 @@ class Command:
             pass
         else:
             x0, y0, x1, y1 = x1, y1, x0, y0
-        
+
         text_sel = ed.get_text_sel()
         if not text_sel:
             msg_status('Text not selected')
             return
-            
+
         text_ab = dlg_input('Emmet abbreviation:', 'div')
         if not text_ab:
             return
-        
+
         text = do_expand_abbrev(text_ab)
         if not text: return
-                                               
+
         do_insert_result(x0, y0, x1, y1, text, text_sel)
-        
+
 
     def expand_abbrev(self):
         text = do_find_expand()
         if not text or ';' not in text:
             msg_status('Cannot find Emmet abbreviation')
             return
-            
+
         slen, text = text.split(';', maxsplit=2)
         nlen = int(slen)
         x0, y0, x1, y1 = ed.get_carets()[0]
